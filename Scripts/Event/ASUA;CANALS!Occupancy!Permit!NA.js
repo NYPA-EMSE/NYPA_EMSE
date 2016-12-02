5296a741347e93d7af1c93c215bfd8b9b8b3bc73
139542 <mlinscheid@accela.com>
2016-12-02 11:12:43 -0600

    addParameter

--- Scripts/Event/ASUA;CANALS!Occupancy!Permit!NA.js
+++ Scripts/Event/ASUA;CANALS!Occupancy!Permit!NA.js
@@ -36,3 +36,42 @@ catch (err) {
 	logDebug("A JavaScript Error occurred: ASUA:CANALS/Occupancy/Permit/NA: #02: " + err.message);
 	logDebug(err.stack)
 }
+
+/**************************************************************************************************************
+*	ID-3 - Send Email Canceled
+*	Mike Linscheid
+*/
+try{
+	if (capStatus == "Canceled") {
+		var emailTemplateName = "CANAL_STATUS_CHANGE"
+		applicantName = "-NA-"
+		conArr = getContactArray();
+		for (c in conArr) {
+			if (conArr[c]["contactType"] == "Applicant" ) {
+				applicantName = conArr[c]["firstName"] + " " + conArr[c]["lastName"]
+				break
+			}
+		}
+		
+		var eParams = aa.util.newHashtable(); 
+		addParameter(eParams, "$$alias$$ ", cap.getCapType().getAlias())
+		addParameter(eParams, "$$altId$$",altId)
+		addParameter(eParams, "$$applicantName$$", applicantName)
+		addParameter(eParams, "$$status$$", capStatus)
+		addParameter(eParams, "$$userId$$", currentUserID)
+		
+		DLMemailList = getUserEmailsByTitle("Director of Land Management")
+		PAemailList = getUserEmailsByTitle("Permit Administrator")
+		
+		for (e in DLMemailList) sendNotification(sysFromEmail, DLMemailList[e], "", emailTemplateName, eParams, null)
+		for (e in PAemailList) sendNotification(sysFromEmail, PAemailList[e], "", emailTemplateName, eParams, null)
+		
+		//Send to WORKFLOW People
+		sendNotification(sysFromEmail, getTaskCompletersEmail("Application Entry",getParent()), "", emailTemplateName, eParams, null)
+		sendNotification(sysFromEmail, getTaskCompletersEmail("DPE Complete",getParent()), "", emailTemplateName, eParams, null)
+	}
+}
+catch (err) {
+	logDebug("A JavaScript Error occurred: ASUA:CANALS/Occupancy/Permit/NA: #03: " + err.message);
+	logDebug(err.stack)
+}
