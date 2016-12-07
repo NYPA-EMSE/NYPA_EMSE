95bbc2849dcce91cec6574be6859d179704579ad
139542 <mlinscheid@accela.com>
2016-12-07 10:45:13 -0600

    copyPeopleWithRemove

---
+++ INCLUDES_CUSTOM/copyPeopleWithRemove.js
@@ -0,0 +1,84 @@
+function copyPeopleWithRemove(srcCapId, targetCapId)
+{
+	//1. Get people with source CAPID.
+	var capPeoples = getPeople(srcCapId);
+	if (capPeoples == null || capPeoples.length == 0)
+	{
+		return;
+	}
+	
+	// get all the contacts on the target and delete them.
+	var capContactResult = aa.people.getCapContactByCapID(targetCapId);
+	if (capContactResult.getSuccess()) {
+		var Contacts = capContactResult.getOutput();
+		for (yy in Contacts) {
+			var con = Contacts[yy];
+			var capContactType = con.getPeople().getContactType();
+			var capContactId = con.getPeople().getContactSeqNumber();
+			delResult = aa.people.removeCapContact(targetCapId, capContactId);
+			if (!delResult.getSuccess()) {
+				logDebug("Error removing contacts on target Cap " + delResult.getErrorMessage());
+			}
+		}
+	}
+	
+	
+	for (loopk in capPeoples) {
+		sourcePeopleModel = capPeoples[loopk];
+		//3.1 Set target CAPID to source people.
+		sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
+		aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
+	}
+}
+
+
+function getPeople(capId)
+{
+	capPeopleArr = null;
+	var s_result = aa.people.getCapContactByCapID(capId);
+	if(s_result.getSuccess())
+	{
+		capPeopleArr = s_result.getOutput();
+		if(capPeopleArr != null || capPeopleArr.length > 0)
+		{
+			for (loopk in capPeopleArr)	
+			{
+				var capContactScriptModel = capPeopleArr[loopk];
+				var capContactModel = capContactScriptModel.getCapContactModel();
+				var peopleModel = capContactScriptModel.getPeople();
+				var contactAddressrs = aa.address.getContactAddressListByCapContact(capContactModel);
+				if (contactAddressrs.getSuccess())
+				{
+					var contactAddressModelArr = convertContactAddressModelArr(contactAddressrs.getOutput());
+					peopleModel.setContactAddressList(contactAddressModelArr);    
+				}
+			}
+		}
+		
+		else
+		{
+			logDebug("WARNING: no People on this CAP:" + capId);
+			capPeopleArr = null;
+		}
+	}
+	else
+	{
+		logDebug("ERROR: Failed to People: " + s_result.getErrorMessage());
+		capPeopleArr = null;	
+	}
+	return capPeopleArr;
+}
+function convertContactAddressModelArr(contactAddressScriptModelArr)
+{
+	var contactAddressModelArr = null;
+	if(contactAddressScriptModelArr != null && contactAddressScriptModelArr.length > 0)
+	{
+		contactAddressModelArr = aa.util.newArrayList();
+		for(loopk in contactAddressScriptModelArr)
+		{
+			contactAddressModelArr.add(contactAddressScriptModelArr[loopk].getContactAddressModel());
+		}
+	}	
+	return contactAddressModelArr;
+}
+
