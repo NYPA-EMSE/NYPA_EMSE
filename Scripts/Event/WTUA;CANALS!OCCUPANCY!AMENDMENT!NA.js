c87a528d644f67d7c8f4a9ab34bbade249666731
139542 <mlinscheid@accela.com>
2016-12-09 11:36:35 -0600

    Revised email

--- Scripts/Event/WTUA;CANALS!OCCUPANCY!AMENDMENT!NA.js
+++ Scripts/Event/WTUA;CANALS!OCCUPANCY!AMENDMENT!NA.js
@@ -19,34 +19,6 @@ try{
 			//aa.cap.copyCapWorkDesInfo(capId, parentId);
 			//aa.cap.copyCapDetailInfo(capId, parentId);
 			//copyAdditionalInfo(capId, parentId);
-		
-			//Script 14 - Revised Email
-			/*
-			var emailTemplateName = "CANAL_STATUS_UPDATE"
-			applicantName = "-NA-"
-			conArr = getContactArray(parentId);
-			for (c in conArr) {
-				if (conArr[c]["contactType"] == "Applicant" ) {
-					applicantName = conArr[c]["firstName"] + " " + conArr[c]["lastName"]
-					break
-				}
-			}
-			
-			parentCap = aa.cap.getCap(parentId).getOutput();
-			
-			var eParams = aa.util.newHashtable(); 
-			addParameter(eParams, "$$alias$$", parentCap.getCapType().getAlias())
-			addParameter(eParams, "$$altId$$",""+parentId.getCustomID())
-			addParameter(eParams, "$$applicantName$$", applicantName)
-			addParameter(eParams, "$$status$$", ""+parentCap.getCapStatus())
-			addParameter(eParams, "$$userId$$", currentUserID)
-			
-			DLMemailList = getUserEmailsByTitle("Director of Land Management")
-			PAemailList = getUserEmailsByTitle("Permit Administrator")
-			
-			for (e in DLMemailList) sendNotification(sysFromEmail, DLMemailList[e], "", emailTemplateName, eParams, null)
-			for (e in PAemailList) sendNotification(sysFromEmail, PAemailList[e], "", emailTemplateName, eParams, null)
-			*/
 		}
 		
 		//Lock Amendment record
@@ -57,3 +29,41 @@ catch (err) {
 	logDebug("A JavaScript Error occurred: WTUA:CANALS/Occupancy/Amendment/NA: #08: " + err.message);
 	logDebug(err.stack)
 }	
+
+
+/**************************************************************************************************************
+*	ID-14 - Send permit revised email
+*	Mike Linscheid
+*/
+try{
+	if (wfTask == "DPE Review" && wfStatus == "Complete") ) {
+		var emailTemplateName = "CANAL_REVISED"
+		applicantName = "-NA-"
+		conArr = getContactArray(parentId);
+		for (c in conArr) {
+			if (conArr[c]["contactType"] == "Applicant" ) {
+				applicantName = conArr[c]["firstName"] + " " + conArr[c]["lastName"]
+				break
+			}
+		}
+		
+		parentCap = aa.cap.getCap(parentId).getOutput();
+		
+		var eParams = aa.util.newHashtable(); 
+		addParameter(eParams, "$$alias$$", parentCap.getCapType().getAlias())
+		addParameter(eParams, "$$altId$$",""+parentId.getCustomID())
+		addParameter(eParams, "$$applicantName$$", applicantName)
+		//addParameter(eParams, "$$status$$", ""+parentCap.getCapStatus())
+		//addParameter(eParams, "$$userId$$", currentUserID)
+		
+		DLMemailList = getUserEmailsByTitle("Director of Land Management")
+		PAemailList = getUserEmailsByTitle("Permit Administrator")
+		
+		for (e in DLMemailList) sendNotification(sysFromEmail, DLMemailList[e], "", emailTemplateName, eParams, null)
+		for (e in PAemailList) sendNotification(sysFromEmail, PAemailList[e], "", emailTemplateName, eParams, null)
+	}
+}
+catch (err) {
+	logDebug("A JavaScript Error occurred: WTUA:CANALS/Occupancy/Amendment/NA: #014: " + err.message);
+	logDebug(err.stack)
+}
