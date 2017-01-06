7eddd4b240c7ff9175073b3bad3be11901d4e879
139542 <mlinscheid@accela.com>
2017-01-06 14:07:30 -0600

    update manual invoice logic

--- Scripts/Event/ASA;CANALS!OCCUPANCY!INVOICE!NA.js
+++ Scripts/Event/ASA;CANALS!OCCUPANCY!INVOICE!NA.js
@@ -3,6 +3,34 @@
 */
 try{
 	addFee("CNL-OC-INV", "CANAL-OC-I", "FINAL", 1, "Y")
+	parentId = getParent()
+	today = new Date()
+	nextInvDate = new Date(today.getTime())
+	try {
+		nextInvDate = new Date(getAppSpecific("Next Invoice Date", parentId))
+	} catch(err){
+		//use this month as default
+	}
+
+	var addMonths = 12
+	switch (""+getAppSpecific("Permit Term", parentId)) {
+		case "ANNUAL":
+			addMonths = 12;
+			break;
+		case "SEMI-ANNUAL":
+			addMonths = 6;
+			break;
+		case "QUARTERLY":
+			addMonths = 3;
+			break;
+		case "MONTHLY":
+			addMonths = 1;
+			break;
+	}
+	nextInvDate.setMonth(nextInvDate.getMonth() + addMonths)
+
+	editAppSpecific("Last Invoice Date", jsDateToASIDate(today))
+	editAppSpecific("Next Invoice Date", jsDateToASIDate(nextInvDate))
 }
 catch (err) {
 	logDebug("A JavaScript Error occurred: ASA:CANALS/Occupancy/Invoice/NA: " + err.message);
