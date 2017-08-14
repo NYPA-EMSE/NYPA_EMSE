7c44659e3a9598a345a391189bc2888da208e70d
Jeff <i1848967@mvrht.com>
2017-08-14 08:43:08 -0500

    test

--- Scripts/Event/ASUA;CANALS!~!~!~.js
+++ Scripts/Event/ASUA;CANALS!~!~!~.js
@@ -1,6 +1,9 @@
+showDebug = true;
 
 if (capStatus == "Void" || capStatus == "Withdrawn" || capStatus == "Denied") 
 {
+	getCompletedByDetails(capId);
+	/*
 	var emailTemplateName = "CANAL_WFCANCELED"
 	var eParams = aa.util.newHashtable();
 	addParameter(eParams, "$$alias$$", cap.getCapType().getAlias());
@@ -12,4 +15,35 @@ if (capStatus == "Void" || capStatus == "Withdrawn" || capStatus == "Denied")
 	{
 		sendNotification("noreply@nypa.com", PAemailList[e], "", emailTemplateName, eParams, null);
 	}
+	*/
 }
+
+function getCompletedByDetails() // option CapId
+{
+	var itemCap = capId
+	if (arguments.length > 0)
+	{
+		itemCap = arguments[0]; // use cap ID specified in args
+	}
+	var cdScriptObjResult = aa.cap.getCapDetail(itemCap);
+	if (cdScriptObjResult.getSuccess())
+	{
+		var cdScriptObj = cdScriptObjResult.getOutput();
+		if (cdScriptObj)
+		{
+			cd = cdScriptObj.getCapDetailModel();
+			debugObject(cd);
+		}
+		else
+		{
+			logDebug("**ERROR: No cap detail script object");
+			return false;
+		}
+	}
+	else
+	{ 
+		logDebug("**ERROR: No cap detail script object : " + cdScriptObjResult.getErrorMessage());
+		return false;
+	}
+}
+ 
