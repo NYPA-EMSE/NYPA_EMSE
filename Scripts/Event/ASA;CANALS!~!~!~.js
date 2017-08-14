d1a0ebce86c5e2272c810b7a3dea083a0b77488a
Jeff <i1848967@mvrht.com>
2017-08-14 09:31:18 -0500

    test

---
+++ Scripts/Event/ASA;CANALS!~!~!~.js
@@ -0,0 +1,44 @@
+showDebug = true;
+getCompletedByEmail(capId);
+/*
+if (capStatus == "Void" || capStatus == "Withdrawn" || capStatus == "Denied") 
+{
+	getCompletedByEmail(capId);
+	
+	var emailTemplateName = "CANAL_WFCANCELED"
+	var eParams = aa.util.newHashtable();
+	addParameter(eParams, "$$alias$$", cap.getCapType().getAlias());
+	addParameter(eParams, "$$altId$$",capIDString);
+	addParameter(eParams, "$$wfStatus$$", capStatus);
+	PAemailList = getUserEmailsByTitle("Permit Administrator");
+	logDebug(PAemailList.join(","));
+	for (e in PAemailList)
+	{
+		sendNotification("noreply@nypa.com", PAemailList[e], "", emailTemplateName, eParams, null);
+	}
+	
+}
+*/
+
+function getCompletedByEmail() // option CapId
+{
+	var compStaff = "";
+	var itemCap = capId
+	if (arguments.length > 0)
+	{
+		itemCap = arguments[0]; // use cap ID specified in args
+	}
+	var capObjResult = aa.cap.getCap(itemCap);
+	if (capObjResult.getSuccess())
+	{
+		var capDet = capObjResult.getOutput();
+		var capMod = capDet.getCapModel();
+		debugObject(capMod);
+	}
+	else
+	{ 
+		logDebug("**ERROR: No cap script object : " + capObjResult.getErrorMessage());
+		return false;
+	}
+}
+ 
