d3d5663cfe132f63b08bf6f6b2ef61bf04803fd6
Jeff <i1848967@mvrht.com>
2017-08-31 09:30:11 -0500

    test

--- Scripts/ASA;CANALS!~!~!~.js
+++ Scripts/ASA;CANALS!~!~!~.js
@@ -1,12 +1,11 @@
 
 showdebug = true;
 var conArray = getContactArray();
-debugObject(conArray);
-
 var conNbr = "";
 
 for (con in conArray)
 {
+	debugObject(conArray[con]);
 	if (conArray[con].contactType == "Billing")
 	{
 		conNbr = conArray[con].contactSeqNumber;
