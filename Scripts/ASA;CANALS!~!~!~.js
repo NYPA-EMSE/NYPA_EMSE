210a144cf90c2a377c5cdc8892d923522680dc6f
Jeff <i1848967@mvrht.com>
2017-08-31 08:31:08 -0500

    test

--- Scripts/ASA;CANALS!~!~!~.js
+++ Scripts/ASA;CANALS!~!~!~.js
@@ -1,10 +1,16 @@
+
+showdebug = true;
 var conArray = getContactArray();
+debugObject(conArray);
+
 var conNbr = "";
+
 for (con in conArray)
 {
 	if (conArray[con].contactType == "Billing")
 	{
 		conNbr = conArray[con].contactSeqNumber;
+		logDebug("Contact number: " + conNbr);
 		if (matches(conNbr, undefined, "", null))
 		{
 			contactSetPrimary(conNbr);
